7bd7ac50d22940b0ab847b3f16c4f5ad381e5470
NETtoSan <netzamora05@gmail.com>
2021-03-22 20:04:12 +0700

    Necessary file structure for text editor

--- system/executables/text.js
+++ system/executables/text.js
@@ -1,6 +1,6 @@
 const Command = require('../base/programs')
 const discord = require('discord.js')
-
+const fs = require('fs')
 class text extends Command{
   constructor(bot) {
     super(bot, {
@@ -8,10 +8,23 @@ class text extends Command{
       description: "A text editor",
       category: "System",
       usage: "!text <file? 'name'/new>",
-      aliases: ["txt"],
+      aliases: ["edit"],
     });
   }
   async run (message,args,bot){
-    return message.channel.send(new discord.MessageEmbed().setTitle("This function is currently in development").setDescription("Please come back later!").setColor(0x33FFEC))
+    let defaultfile = "./text"
+    if(!args[0]) return message.channel.send(new discord.MessageEmbed().setTitle("This function is currently in development").setDescription("Please come back later!").setColor(0x33FFEC))
+    if(args[0] == "new"){
+      return message.channel.send(new discord.MessageEmbed().setTitle("New file function"))
+
+      return
+    }
+    if(args[0]){
+      //cut only dir
+      let titlemsg = message.content.slice(6)
+      let fiS = fs.readFile(`${defaultfile}/${titlemsg}`,"utf8",(err)=>{
+        if(err) return message.channel.send(new discord.MessageEmbed().setTitle(`${titlemsg} doesnt exist`).setDescription("Please try again").setColor(0xFF3333))
+      })
+    }
   }
 }
