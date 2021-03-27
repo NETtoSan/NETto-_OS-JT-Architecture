6c6ee77aee2ba1ba018ad7423330dc354315da4b
NETtoSan <netzamora05@gmail.com>
2021-03-28 00:40:30 +0700

    atom wants me to push it idk

--- system/events/message.js
+++ system/events/message.js
@@ -29,58 +29,66 @@ module.exports = class {
           .setDescription("Otherwise a bot will not work")
           .setColor(0xff3333)
       );
-    const cmd =
-      this.bot.programs.get(command) ||
-      this.bot.programs.get(this.bot.aliases.get(command));
-    if (!cmd)
-      return obsolete(message,this.bot)
-    if (message.guild && !message.member)
-      await message.guild.fetchMember(message.author);
+    try{
+      const cmd =
+        this.bot.programs.get(command) ||
+        this.bot.programs.get(this.bot.aliases.get(command));
+      if (!cmd)
+        return obsolete(message,this.bot)
+      if (message.guild && !message.member)
+        await message.guild.fetchMember(message.author);
 
 
-    //Permission shit goes here
-        const bot = this.bot
-        let perm = cmd.conf.permissions
-        let lw = await message.client.fetchApplication().then(app=>app.owner.id)
-        //Logic shits
-        if(!perm) return message.channel.send(new discord.MessageEmbed().setTitle("Permission error on JS").setDescription("Check ./system/base and ./system/executables").setColor(0xFF3333))
-        if(perm == "default") {
-            return cmd.run(message,args,bot)
-        }
-        if(perm == "OWNER" && message.author.id == lw) {
-            return cmd.run(message,args,bot)
-        }
-        const uperm = await message.member.permissions.has(perm)
-        console.log(uperm)
-        if(uperm) {
-            if(perm == "OWNER") return message.channel.send(new discord.MessageEmbed().setTitle("Access denied!").setDescription(`This program required role of ${perm}`).setColor(0xFF3333))
-            return cmd.run(message,args,bot)
-        }
-        else return message.channel.send(new discord.MessageEmbed().setTitle("Access denied!").setDescription(`This program required role of ${perm}`))
+      //Permission shit goes here
+          const bot = this.bot
+          let perm = cmd.conf.permissions
+          let lw = await message.client.fetchApplication().then(app=>app.owner.id)
+          //Logic shits
+          if(!perm) return message.channel.send(new discord.MessageEmbed().setTitle("Permission error on JS").setDescription("Check ./system/base and ./system/executables").setColor(0xFF3333))
+          if(perm == "default") {
+              return cmd.run(message,args,bot)
+          }
+          if(perm == "OWNER" && message.author.id == lw) {
+              return cmd.run(message,args,bot)
+          }
+          const uperm = await message.member.permissions.has(perm)
+          console.log(uperm)
+          if(uperm) {
+              if(perm == "OWNER") return message.channel.send(new discord.MessageEmbed().setTitle("Access denied!").setDescription(`This program required role of ${perm}`).setColor(0xFF3333))
+              return cmd.run(message,args,bot)
+          }
+          else return message.channel.send(new discord.MessageEmbed().setTitle("Access denied!").setDescription(`This program required role of ${perm}`))
 
+    }
+    catch(e){
+      return message.channel.send(new discord.MessageEmbed().setTitle("oh no").setDescription("Error executing program").addField("Error",`${e}`).setColor(0xFF3333))
+    }
   }
 };
 
-
 function obsolete(message, bot) {
-    const args = message.content.substring(bot.config.prefix.length).split(" ")
-    try {
-        bot.carryover.get(args[0]).execute(message, args, bot)
+    try{
+      const args = message.content.substring(bot.config.prefix.length).split(" ")
+      try {
+          bot.carryover.get(args[0]).execute(message, args, bot)
+      }
+      catch (err) {
+          let reve = bot.programs.map(command => command.help.name)
+          let content = message.content.substring(bot.config.prefix.length).split(" ")
+          var context = `Please try again!`
+          for (const e of reve) {
+              let ARGS = new RegExp(`${e}`)
+              if (content[0].match(ARGS)) {
+                  context = `Do you mean ${e}?`
+              }
+              if (e.includes(content[0])) {
+                  context = `Do you mean ${e}?`
+              }
+          }
+          return message.channel.send(new discord.MessageEmbed().setTitle("No command found!").setDescription(context).setColor(0xFF3333))
+      }
     }
-    catch (err) {
-        let reve = bot.programs.map(command => command.help.name)
-        let content = message.content.substring(bot.config.prefix.length).split(" ")
-        var context = `Please try again!`
-        for (const e of reve) {
-            let ARGS = new RegExp(`${e}`)
-            if (content[0].match(ARGS)) {
-
-                context = `Do you mean ${e}?`
-            }
-            if (e.includes(content[0])) {
-                context = `Do you mean ${e}?`
-            }
-        }
-        return message.channel.send(new discord.MessageEmbed().setTitle("No command found!").setDescription(context).setColor(0xFF3333))
+    catch(e){
+      return message.channel.send(new discord.MessageEmbed().setTitle("Error executing program").setDescription("Something went wrong while executing old programs").addField("Error",`${e}`).setColor(0xFF3333))
     }
 }
