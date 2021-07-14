ae6bd11d36efa09f40c3b81ac04841124b3b751b
NETtoSan <netzamora05@gmail.com>
2021-07-14 11:35:21 +0700

    e

--- system/programs/permParsing.js
+++ system/programs/permParsing.js
@@ -1,10 +1,19 @@
 class permParsing{
   constructor(bot){
-
+    this.validiator = []
+    this.bot
   }
 
-  check(message,args,bot,app){
-
+  async check(message,args,bot,app){
+    this.bot = bot
+    const esfx = async () =>{
+      const permexec = await this.permparse(app,a,b)
+      if(permexec){
+        switch(permexec){
+          
+        }
+      }
+    }
   }
 }
 
